d591f563a6e94098e69e907fb169be35d6362ec6
Raymundo <A00570654@itesm.mx>
2021-12-10 11:12:58 +0000

    HTTPS implemented

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000;
@@ -8,87 +7,107 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-// Part #1 (60%) 
-
-app.get('/add/:x/:y/',  (req, res) => {
+// Part #1 (60%) - URL 
+// Addition
+app.get('/add/:x/:y/', (req, res) => {
+    // Get numbers
     const x = parseFloat(req.params.x);
     const y = parseFloat(req.params.y);
 
-    res.header('Content-Type', 'text/plain')
+    // Do the operation
+    res.setHeader('Content-Type', 'text/plain');
     const result = x + y;
+    
 
+    // Is the result a number?
     if (isNaN(result)) {
-        res.sendStatus(500);
+        res.status(500).send('Invalid type of parameters');
     } else {
-        res.send(`HTTP/1.1 200 OK \nContent-Type: text/plain\n\n${result}`);
+        res.send(`HTTP/1.1 200 OK\nContent-Type: text/plain\n\n${result}`);
     }
 })
 app.get('/add/*', (req, res) => {
-    res.status(500);
-    res.send(`Invalid number of parameters`);
+    res.status(500).send('Invalid number of parameters');
 })
 
-app.get('/substract/:x/:y/',  (req, res) => {
+
+// Substraction
+app.get('/substract/:x/:y/', (req, res) => {
+    // Get numbers
     const x = parseFloat(req.params.x);
     const y = parseFloat(req.params.y);
+
+    // Do the operation
+    res.setHeader('Content-Type', 'text/plain');
     const result = x - y;
+    
 
-    res.header('Content-Type', 'text/plain')
-
+    // Is the result a number?
     if (isNaN(result)) {
-        res.sendStatus(500);
+        res.status(500).send('Invalid type of parameters');
     } else {
-        res.send(`HTTP/1.1 200 OK \nContent-Type: text/plain\n\n${result}`);
+        res.send(`HTTP/1.1 200 OK\nContent-Type: text/plain\n\n${result}`);
     }
 })
 app.get('/substract/*', (req, res) => {
-    res.status(500);
-    res.send(`Invalid number of parameters`);
+    res.status(500).send('Invalid number of parameters');
 })
 
-app.get('/multiply/:x/:y/',  (req, res) => {
+
+// Multiplication
+app.get('/multiply/:x/:y/', (req, res) => {
+    // Get numbers
     const x = parseFloat(req.params.x);
     const y = parseFloat(req.params.y);
+
+    // Do the operation
+    res.setHeader('Content-Type', 'text/plain');
     const result = x * y;
+    
 
-    res.header('Content-Type', 'text/plain')
-
+    // Is the result a number?
     if (isNaN(result)) {
-        res.sendStatus(500);
+        res.status(500).send('Invalid type of parameters');
     } else {
-        res.send(`HTTP/1.1 200 OK \nContent-Type: text/plain\n\n${result}`);
+        res.send(`HTTP/1.1 200 OK\nContent-Type: text/plain\n\n${result}`);
     }
 })
 app.get('/multiply/*', (req, res) => {
-    res.status(500);
-    res.send(`Invalid number of parameters`);
+    res.status(500).send('Invalid number of parameters');
 })
 
-app.get('/divide/:x/:y/',  (req, res) => {
+
+// Division
+app.get('/divide/:x/:y/', (req, res) => {
+    // Get numbers
     const x = parseFloat(req.params.x);
     const y = parseFloat(req.params.y);
+
+    // Divide by zero?
     if (y == 0) {
-        res.sendStatus(500);
+        res.status(500).send('Cannot divide by zero');
         return;
     }
 
-    res.header('Content-Type', 'text/plain')
-
+    // Do the operation
+    res.setHeader('Content-Type', 'text/plain');
     const result = x / y;
     
+    
+    // Is the result a number?
     if (isNaN(result)) {
-        res.sendStatus(500);
+        res.status(500).send('Invalid type of parameters');
     } else {
-        res.send(`HTTP/1.1 200 OK \nContent-Type: text/plain\n\n${result}`);
+        res.send(`HTTP/1.1 200 OK\nContent-Type: text/plain\n\n${result}`);
     }
 })
 app.get('/divide/*', (req, res) => {
-    res.status(500);
-    res.send(`Invalid number of parameters`);
+    res.status(500).send('Invalid number of parameters');
 })
 
-// Part #2 (30%) 
 
+// Part #2 (30%) - JSON
+// Operation to perform
 function calc(op,x,y) {
     switch(op) {
         case 'add': return x + y; 
@@ -99,43 +118,48 @@ function calc(op,x,y) {
     } 
 }
 
+// JSON Function
 app.post('/', (req, res) => {
-
+    // Is JSON formed correctly?
     if (!req.body.operation || !req.body.arguments) {
-        res.status(500);
-        res.send(`Malformed JSON request`);
-        return;
-    }
-
-    const op = req.body.operation;
-
-    if (req.body.arguments.length != 2) {
-        res.status(500);
-        res.send(`Invalid number of parameters`);
+        res.status(500).send('Malformed JSON request');
         return;
     }
     
+    // Get operation to perform
+    const op = req.body.operation;
+
+    // How many numbers we received?
+    if (req.body.arguments.length != 2) {
+        res.status(500).send('Invalid number of parameters');
+        return;
+    }
+    
+    // Get numbers
     const x = req.body.arguments[0];
     const y = req.body.arguments[1];
 
+    // Are numbers valid?
     if (isNaN(x) || isNaN(y)) {
-        res.status(500);
-        res.send(`Parameters must be numbers`);
+        res.status(500).send('Invalid type of parameters');
         return;
     }
 
+    // Divide by zero?
     if (req.body.operation == 'divide') {
         if(y == 0) {
-            res.status(500);
-            res.send(`Cannot divide by zero`);
+            res.status(500).send('Cannot divide by zero');
             return;
         }
     }
 
+    // Do the operation?
     const result = calc(op,x,y);
+    res.setHeader('Content-Type', 'application/json');
 
+    // Is the result a number?
     if (isNaN(result)) {
-        res.sendStatus(500);
+        res.status(500).send('Invalid type of parameters');
     } else {
         res.send(`HTTP/1.1 200 OK\n Content-Type: application/json\n\n{\n "result": ${result} \n}`);
     }
